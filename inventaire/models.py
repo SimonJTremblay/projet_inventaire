1d2e09f09fa7c303cff6471653f630cb1b66d8d4
SimonJTremblay <stremblay@coach.ca>
2020-03-24 12:19:47 -0400

    added users app

--- inventaire/models.py
+++ inventaire/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Produit(models.Model):
+    Nom = models.CharField(max_length=100)
+    Description = models.TextField()
+    Type = models.CharField(max_length=50)
+    QuantiteStock = models.IntegerField()
+    SeuilMinimum = models.IntegerField()
+
+    def __str__(self):
+        return self.Nom
+    
--- inventaire/views.py
+++ inventaire/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
+from .models import Produit
 
-
-produits = [
-    {
-        'Nom': 'clous - 1\"',
-        'Description': 'boite de 20 clous de 1\"',
-        'Type': 'materiel',
-        'QuantiteStock': '200',
-        'SeuilMinimum': '50',
-    },
-    {
-        'Nom': 'vis - 1\"',
-        'Description': 'boite de 30 vis de 1\"',
-        'Type': 'materiel',
-        'QuantiteStock': '125',
-        'SeuilMinimum': '50',
-    }
-]
 
 def home(request):
+    """ renders inventaire/home.html"""
     context = {
-        'produits': produits
+        'produits': Produit.objects.all()
     }
     return render(request, 'inventaire/home.html', context)
 
